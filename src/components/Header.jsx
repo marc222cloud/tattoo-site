192dddb5756014a98dabf6f55a7b650623f06d52
marc222cloud <marcantonio.gahan@gmail.com>
2026-05-17 17:03:04 -0400

    feat: rebuild header UI to match Archangel design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 import './Header.css';
+import logo from '../assets/tattoologo1.png';
 
 export default function Header() {
   return (
     <header>
       <div className="header-inner">
         {/* Left: Brand / Logo */}
-        <h1 className="header-brand">Archangel Tattoo & Piercing</h1>
+        <div className="header-brand">
+          <img src={logo} alt="Archangel Tattoo & Piercing" />
+        </div>
 
         {/* Right: Nav + CTA */}
         <nav className="header-nav" aria-label="Primary">
           <a href="#home" className="header-link">Home</a>
+          <span> | </span>
           <a href="#artists" className="header-link">Artists</a>
+          <span> | </span>
           <a href="#portfolio" className="header-link">Portfolio</a>
+          <span> | </span>
           <a href="#about" className="header-link">About</a>
           <a href="#booking" className="header-link header-cta">Book Now</a>
         </nav>
